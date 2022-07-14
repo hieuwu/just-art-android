05440685debd060e96aecd92b035fc36aaf792e7
Hieu Vu <star.hieu.99@gmail.com>
2022-07-14 22:39:19 +0700

    Create use case

--- app/src/main/java/com/hieuwu/justart/domain/usecases/RetrieveArtWorksUseCase.kt
+++ app/src/main/java/com/hieuwu/justart/domain/usecases/RetrieveArtWorksUseCase.kt
@@ -1,4 +1,12 @@
 package com.hieuwu.justart.domain.usecases
 
-interface RetrieveArtWorksUseCase {
+interface RetrieveArtWorksUseCase :
+    UseCase<RetrieveArtWorksUseCase.Input, RetrieveArtWorksUseCase.Result> {
+    class Input
+    class Result(
+        val data: String?, val error: RetrieveArtWorksUseCase.Error?,
+    )
+
+    data class Error(val type: ErrorType)
+    class ErrorType
 }
