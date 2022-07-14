79b4f39f4759ee53072218f638a9ae6fc9e82bb6
Hieu Vu <star.hieu.99@gmail.com>
2022-07-14 21:41:14 +0700

    Setup nav bar

--- app/src/main/java/com/hieuwu/justart/MainActivity.kt
+++ app/src/main/java/com/hieuwu/justart/MainActivity.kt
@@ -2,10 +2,21 @@ package com.hieuwu.justart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.appNavHostFragment
+        ) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.setupWithNavController(navController)
+
     }
 }
