cfe5796264a0f0eb9f73ba88719aec6c1424b5ec
Hieu Vu <star.hieu.99@gmail.com>
2022-07-15 23:38:57 +0700

    Update result class

--- app/src/main/java/com/hieuwu/justart/domain/usecases/RetrieveArtWorksUseCase.kt
+++ app/src/main/java/com/hieuwu/justart/domain/usecases/RetrieveArtWorksUseCase.kt
@@ -3,9 +3,10 @@ package com.hieuwu.justart.domain.usecases
 interface RetrieveArtWorksUseCase :
     UseCase<RetrieveArtWorksUseCase.Input, RetrieveArtWorksUseCase.Result> {
     class Input
-    class Result(
-        val data: String?, val error: RetrieveArtWorksUseCase.Error?,
-    )
+    open class Result {
+        data class Success(val data: String?) : Result()
+        data class Failure(val error: Error?) : Result()
+    }
 
     data class Error(val type: ErrorType)
     class ErrorType
