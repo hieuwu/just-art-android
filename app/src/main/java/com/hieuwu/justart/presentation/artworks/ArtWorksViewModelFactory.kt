4ea7515f0189a07d49e01566adf3483a66d7ee23
Hieu Vu <star.hieu.99@gmail.com>
2022-07-18 06:53:27 +0700

    Refactor viewmodel factory

--- app/src/main/java/com/hieuwu/justart/presentation/artworks/ArtWorksViewModelFactory.kt
+++ app/src/main/java/com/hieuwu/justart/presentation/artworks/ArtWorksViewModelFactory.kt
@@ -2,12 +2,13 @@ package com.hieuwu.justart.presentation.artworks
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.hieuwu.justart.domain.usecases.RetrieveArtWorksUseCase
 
-class ArtWorksViewModelFactory() : ViewModelProvider.Factory {
+class ArtWorksViewModelFactory(private val retrieveArtWorksUseCase: RetrieveArtWorksUseCase) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(ArtWorksViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
-            return ArtWorksViewModel() as T
+            return ArtWorksViewModel(retrieveArtWorksUseCase) as T
         }
         throw IllegalArgumentException("Unable to construct viewmodel")
     }
