db95760097e99ad1ce9cfe41f11bad312b477082
Hieu Vu <star.hieu.99@gmail.com>
2022-07-18 06:54:11 +0700

    Refactor use case

--- app/src/main/java/com/hieuwu/justart/domain/usecases/RetrieveArtWorksUseCase.kt
+++ app/src/main/java/com/hieuwu/justart/domain/usecases/RetrieveArtWorksUseCase.kt
@@ -1,10 +1,13 @@
 package com.hieuwu.justart.domain.usecases
 
+import com.hieuwu.justartsdk.ApiResult
+import com.hieuwu.justartsdk.artworks.v1.dto.ArtWorksListDto
+
 interface RetrieveArtWorksUseCase :
     UseCase<RetrieveArtWorksUseCase.Input, RetrieveArtWorksUseCase.Result> {
-    class Input
+    class Input()
     open class Result {
-        data class Success(val data: String?) : Result()
+        data class Success(val data: ApiResult<ArtWorksListDto>?) : Result()
         data class Failure(val error: Error?) : Result()
     }
 
--- app/src/main/java/com/hieuwu/justart/domain/usecases/impl/RetrieveArtWorksUseCaseImpl.kt
+++ app/src/main/java/com/hieuwu/justart/domain/usecases/impl/RetrieveArtWorksUseCaseImpl.kt
@@ -1,9 +1,21 @@
 package com.hieuwu.justart.domain.usecases.impl
 
 import com.hieuwu.justart.domain.usecases.RetrieveArtWorksUseCase
+import com.hieuwu.justartsdk.ApiResult
+import com.hieuwu.justartsdk.artworks.v1.ArtWorksService
+import com.hieuwu.justartsdk.artworks.v1.dto.ArtWorksListDto
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
-class RetrieveArtWorksUseCaseImpl : RetrieveArtWorksUseCase {
+class RetrieveArtWorksUseCaseImpl @Inject constructor(
+    private val artWorksService: ArtWorksService
+) : RetrieveArtWorksUseCase {
     override suspend fun execute(input: RetrieveArtWorksUseCase.Input): RetrieveArtWorksUseCase.Result {
-        return RetrieveArtWorksUseCase.Result(null, null)
+        val res: ApiResult<ArtWorksListDto>
+        withContext(Dispatchers.IO) {
+            res = artWorksService.getArtWorks()
+        }
+        return RetrieveArtWorksUseCase.Result.Success(res)
     }
 }
