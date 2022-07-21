1c66138b3137ad0fdf89c5af1702024ebbf7dc54
Hieu Vu <star.hieu.99@gmail.com>
2022-07-21 22:11:06 +0700

    Update return type

--- app/src/main/java/com/hieuwu/justart/domain/usecases/RetrieveArtWorksUseCase.kt
+++ app/src/main/java/com/hieuwu/justart/domain/usecases/RetrieveArtWorksUseCase.kt
@@ -1,13 +1,14 @@
 package com.hieuwu.justart.domain.usecases
 
 import com.hieuwu.justartsdk.ApiResult
+import com.hieuwu.justartsdk.artworks.v1.domain.ArtWork
 import com.hieuwu.justartsdk.artworks.v1.dto.ArtWorksListDto
 
 interface RetrieveArtWorksUseCase :
     UseCase<RetrieveArtWorksUseCase.Input, RetrieveArtWorksUseCase.Result> {
     class Input()
     open class Result {
-        data class Success(val data: ApiResult<ArtWorksListDto>?) : Result()
+        data class Success(val data: List<ArtWork>?) : Result()
         data class Failure(val error: Error?) : Result()
     }
 
