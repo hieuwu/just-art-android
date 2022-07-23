5a6e40092dcbd10b6b287cb8850c9f7b5f5a4c7d
Hieu Vu <star.hieu.99@gmail.com>
2022-07-23 22:22:56 +0700

    Remove unused imports

--- app/src/main/java/com/hieuwu/justart/presentation/artworks/ArtWorksFragment.kt
+++ app/src/main/java/com/hieuwu/justart/presentation/artworks/ArtWorksFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.os.bundleOf
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
