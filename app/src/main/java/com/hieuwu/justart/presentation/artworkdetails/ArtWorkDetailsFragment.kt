f013d6d7dce2a43e4c5871f7c0a22176cfd34723
Hieu Vu <star.hieu.99@gmail.com>
2022-07-23 15:31:57 +0700

    Refactor details fragment

--- app/src/main/java/com/hieuwu/justart/presentation/artworkdetails/ArtWorkDetailsFragment.kt
+++ app/src/main/java/com/hieuwu/justart/presentation/artworkdetails/ArtWorkDetailsFragment.kt
@@ -5,56 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.hieuwu.justart.R
+import com.hieuwu.justart.databinding.FragmentArtWorksBinding
+import com.hieuwu.justart.databinding.FragmentArtworkDetailsBinding
+import com.hieuwu.justart.domain.usecases.RetrieveArtWorkDetailsUseCase
+import com.hieuwu.justart.presentation.artworks.ArtWorksViewModel
+import com.hieuwu.justart.presentation.artworks.ArtWorksViewModelFactory
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ArtDetailsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+@AndroidEntryPoint
 class ArtDetailsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    @Inject
+    lateinit var retrieveArtWorkDetailsUseCase: RetrieveArtWorkDetailsUseCase
+
+
+    private lateinit var binding: FragmentArtworkDetailsBinding
+
+    private lateinit var viewModel: ArtWorkDetailsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_art_details, container, false)
-    }
+        binding = FragmentArtworkDetailsBinding.inflate(inflater, container, false)
+        val viewModelFactory = ArtWorkDetailsViewModelFactory(retrieveArtWorkDetailsUseCase)
+        viewModel = ViewModelProvider(this, viewModelFactory)[ArtWorkDetailsViewModel::class.java]
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ArtDetailsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ArtDetailsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        viewModel.artWorksDetails?.observe(viewLifecycleOwner){
+            val a = it
+        }
+
+        return binding.root
     }
 }
