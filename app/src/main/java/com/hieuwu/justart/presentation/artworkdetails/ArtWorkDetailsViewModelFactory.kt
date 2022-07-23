cf8ee12629ac87e44c2dcd4ec2dc76947fa65df6
Hieu Vu <star.hieu.99@gmail.com>
2022-07-23 15:31:42 +0700

    Add viewmodel factory

--- app/src/main/java/com/hieuwu/justart/presentation/artworkdetails/ArtWorkDetailsViewModelFactory.kt
+++ app/src/main/java/com/hieuwu/justart/presentation/artworkdetails/ArtWorkDetailsViewModelFactory.kt
@@ -1,4 +1,16 @@
 package com.hieuwu.justart.presentation.artworkdetails
 
-class ArtWorkDetailsViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.hieuwu.justart.domain.usecases.RetrieveArtWorkDetailsUseCase
+
+class ArtWorkDetailsViewModelFactory(private val retrieveArtWorkDetailsUseCase: RetrieveArtWorkDetailsUseCase) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ArtWorkDetailsViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return ArtWorkDetailsViewModel(retrieveArtWorkDetailsUseCase) as T
+        }
+        throw IllegalArgumentException("Unable to construct viewmodel")
+    }
 }
