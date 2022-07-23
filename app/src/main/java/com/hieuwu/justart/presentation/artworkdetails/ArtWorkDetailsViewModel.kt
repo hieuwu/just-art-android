da436a41a4487f6c4fe684629cd20280718ab75e
Hieu Vu <star.hieu.99@gmail.com>
2022-07-23 15:32:04 +0700

    Add view model

--- app/src/main/java/com/hieuwu/justart/presentation/artworkdetails/ArtWorkDetailsViewModel.kt
+++ app/src/main/java/com/hieuwu/justart/presentation/artworkdetails/ArtWorkDetailsViewModel.kt
@@ -1,4 +1,33 @@
 package com.hieuwu.justart.presentation.artworkdetails
 
-class ArtWorkDetailsViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.hieuwu.justart.domain.models.ArtWorkDetailsDo
+import com.hieuwu.justart.domain.usecases.RetrieveArtWorkDetailsUseCase
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class ArtWorkDetailsViewModel @Inject constructor(
+    private val retrieveArtWorkDetailsUseCase: RetrieveArtWorkDetailsUseCase
+) : ViewModel() {
+
+    private val _artWorkDetails: MutableLiveData<ArtWorkDetailsDo?>? = null
+    val artWorksDetails: LiveData<ArtWorkDetailsDo?>?
+        get() = _artWorkDetails
+
+    init {
+        getArtWorkDetails()
+    }
+
+    private fun getArtWorkDetails() {
+        viewModelScope.launch {
+            when (val res = retrieveArtWorkDetailsUseCase.execute(RetrieveArtWorkDetailsUseCase.Input(1))) {
+                is RetrieveArtWorkDetailsUseCase.Result.Success -> {
+                    _artWorkDetails?.value = res.data
+                }
+            }
+        }
+    }
 }
