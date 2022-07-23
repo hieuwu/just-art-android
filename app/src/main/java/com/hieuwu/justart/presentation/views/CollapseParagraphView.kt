adb91e2b26ed08a2df12496db3f7a3a6ef3042bb
Hieu Vu <star.hieu.99@gmail.com>
2022-07-23 22:22:46 +0700

    Implement custom view

--- app/src/main/java/com/hieuwu/justart/presentation/views/CollapseParagraphView.kt
+++ app/src/main/java/com/hieuwu/justart/presentation/views/CollapseParagraphView.kt
@@ -1,4 +1,37 @@
 package com.hieuwu.justart.presentation.views
 
-class CollapseParagraphView {
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.hieuwu.justart.R
+import timber.log.Timber
+
+
+@SuppressLint("ViewConstructor")
+class CollapseParagraphView(
+    context: Context,
+    attrs: AttributeSet?,
+    defStyleAttr: Int,
+    defStyleRes: Int
+) : ConstraintLayout(context, attrs, defStyleAttr, defStyleRes) {
+    constructor(
+        context: Context,
+        attrs: AttributeSet?
+    ) : this(context = context, attrs = attrs, 0, 0) {
+        inflateLayout()
+    }
+
+    private fun inflateLayout() {
+        val view =
+            LayoutInflater.from(context).inflate(R.layout.custom_collapse_paragraph_view, this)
+        val title = view.findViewById<TextView>(R.id.titleText)
+        title.setOnClickListener {
+            Timber.d("Tittle click")
+        }
+
+    }
 }
