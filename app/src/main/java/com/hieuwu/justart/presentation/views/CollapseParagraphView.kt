106edab48a0662cee66893fec6192dff247c21c9
Hieu Vu <star.hieu.99@gmail.com>
2022-07-24 23:20:24 +0700

    Simplify view

--- app/src/main/java/com/hieuwu/justart/presentation/views/CollapseParagraphView.kt
+++ app/src/main/java/com/hieuwu/justart/presentation/views/CollapseParagraphView.kt
@@ -4,34 +4,50 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.withStyledAttributes
+import androidx.databinding.DataBindingUtil
 import com.hieuwu.justart.R
-import timber.log.Timber
+import com.hieuwu.justart.databinding.CustomCollapseParagraphViewBinding
 
 
 @SuppressLint("ViewConstructor")
 class CollapseParagraphView(
     context: Context,
     attrs: AttributeSet?,
-    defStyleAttr: Int,
-    defStyleRes: Int
-) : ConstraintLayout(context, attrs, defStyleAttr, defStyleRes) {
-    constructor(
-        context: Context,
-        attrs: AttributeSet?
-    ) : this(context = context, attrs = attrs, 0, 0) {
-        inflateLayout()
+) : ConstraintLayout(context, attrs) {
+
+    private var title: String? = null
+    private var content: String? = null
+
+    private var titleTextView: TextView? = null
+    private var contentTextView: TextView? = null
+
+    init {
+        val view = inflate(
+            context,
+            R.layout.custom_collapse_paragraph_view, this
+        )
+
+        titleTextView = view.findViewById(R.id.titleText)
+        contentTextView = view.findViewById(R.id.paragraphText)
+
+        context.withStyledAttributes(attrs, R.styleable.CollapseParagraphView) {
+            titleTextView?.text = getString(R.styleable.CollapseParagraphView_titleValue)
+            contentTextView?.text = getString(R.styleable.CollapseParagraphView_contentValue)
+        }
     }
 
-    private fun inflateLayout() {
-        val view =
-            LayoutInflater.from(context).inflate(R.layout.custom_collapse_paragraph_view, this)
-        val title = view.findViewById<TextView>(R.id.titleText)
-        title.setOnClickListener {
-            Timber.d("Tittle click")
+    fun setTitle(text: String?) {
+        text?.let {
+            titleTextView?.text = text
         }
+    }
 
+    fun setContent(text: String?) {
+        text?.let {
+            contentTextView?.text = text
+        }
     }
 }
