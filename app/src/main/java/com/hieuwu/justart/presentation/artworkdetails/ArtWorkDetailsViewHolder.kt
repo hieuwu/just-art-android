304f2d593ef4ea1812ca5d74b06794e33801f045
Hieu Vu <star.hieu.99@gmail.com>
2022-07-27 22:00:19 +0700

    Add view holders

--- app/src/main/java/com/hieuwu/justart/presentation/artworkdetails/ArtWorkDetailsViewHolder.kt
+++ app/src/main/java/com/hieuwu/justart/presentation/artworkdetails/ArtWorkDetailsViewHolder.kt
@@ -1,4 +1,45 @@
 package com.hieuwu.justart.presentation.artworkdetails
 
-class ArtWorkDetailsViewHolder {
+import android.view.LayoutInflater
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
+import com.hieuwu.justart.databinding.LayoutArtWorksItemBinding
+import com.hieuwu.justart.databinding.LayoutArtworkDetailsSectionViewBinding
+import com.hieuwu.justart.databinding.LayoutArworkDetailsCollapseSectionViewBinding
+import com.hieuwu.justart.presentation.artworks.ArtWorksAdapter
+import kotlinx.coroutines.NonDisposableHandle.parent
+
+open class ArtWorkDetailsViewHolder(private var binding: ViewBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(artWorkDetailDisplay: ArtWorkDetailDisplay) {
+        when (binding) {
+            is LayoutArworkDetailsCollapseSectionViewBinding -> {
+                val bi = binding as LayoutArworkDetailsCollapseSectionViewBinding
+                bi.displayItem = artWorkDetailDisplay as ArtWorkDetailDisplay.CollapseSection
+                bi.executePendingBindings()
+            }
+        }
+//        when (artWorkDetailDisplay) {
+//            is ArtWorkDetailDisplay.Thumbnail -> {
+//
+//            }
+//            is ArtWorkDetailDisplay.Text -> {
+//
+//            }
+//            is ArtWorkDetailDisplay.Section -> {
+//
+//            }
+//            is ArtWorkDetailDisplay.CollapseSection -> {
+//
+//            }
+//        }
+    }
+
+    class ThumbnailViewHolder(private var binding: ViewBinding) : ArtWorkDetailsViewHolder(binding)
+    class TextViewHolder(private var binding: ViewBinding) : ArtWorkDetailsViewHolder(binding)
+    class SectionViewHolder(private var binding: ViewBinding) : ArtWorkDetailsViewHolder(binding)
+    class CollapseSectionViewHolder(private var binding: ViewBinding) :
+        ArtWorkDetailsViewHolder(binding)
+
 }
