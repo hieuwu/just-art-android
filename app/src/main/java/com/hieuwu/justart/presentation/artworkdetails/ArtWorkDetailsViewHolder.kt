7aa8ea3da4a375707f2ec771a9e4af799355f44d
Hieu Vu <star.hieu.99@gmail.com>
2022-07-27 23:02:05 +0700

    Refactor view holder

--- app/src/main/java/com/hieuwu/justart/presentation/artworkdetails/ArtWorkDetailsViewHolder.kt
+++ app/src/main/java/com/hieuwu/justart/presentation/artworkdetails/ArtWorkDetailsViewHolder.kt
@@ -1,45 +1,49 @@
 package com.hieuwu.justart.presentation.artworkdetails
 
-import android.view.LayoutInflater
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
-import com.hieuwu.justart.databinding.LayoutArtWorksItemBinding
 import com.hieuwu.justart.databinding.LayoutArtworkDetailsSectionViewBinding
+import com.hieuwu.justart.databinding.LayoutArtworkDetailsThumbnailViewBinding
+import com.hieuwu.justart.databinding.LayoutArtworkDetailsTitleViewBinding
 import com.hieuwu.justart.databinding.LayoutArworkDetailsCollapseSectionViewBinding
-import com.hieuwu.justart.presentation.artworks.ArtWorksAdapter
-import kotlinx.coroutines.NonDisposableHandle.parent
 
-open class ArtWorkDetailsViewHolder(private var binding: ViewBinding) :
+abstract class ArtWorkDetailsViewHolder(private var binding: ViewBinding) :
     RecyclerView.ViewHolder(binding.root) {
+    abstract fun bind(artWorkDetailDisplay: ArtWorkDetailDisplay)
 
-    fun bind(artWorkDetailDisplay: ArtWorkDetailDisplay) {
-        when (binding) {
-            is LayoutArworkDetailsCollapseSectionViewBinding -> {
-                val bi = binding as LayoutArworkDetailsCollapseSectionViewBinding
-                bi.displayItem = artWorkDetailDisplay as ArtWorkDetailDisplay.CollapseSection
-                bi.executePendingBindings()
-            }
+    class ThumbnailViewHolder(private var binding: ViewBinding) :
+        ArtWorkDetailsViewHolder(binding) {
+        override fun bind(artWorkDetailDisplay: ArtWorkDetailDisplay) {
+            val bi = binding as LayoutArtworkDetailsThumbnailViewBinding
+            bi.displayItem = artWorkDetailDisplay as ArtWorkDetailDisplay.Thumbnail
+            bi.executePendingBindings()
         }
-//        when (artWorkDetailDisplay) {
-//            is ArtWorkDetailDisplay.Thumbnail -> {
-//
-//            }
-//            is ArtWorkDetailDisplay.Text -> {
-//
-//            }
-//            is ArtWorkDetailDisplay.Section -> {
-//
-//            }
-//            is ArtWorkDetailDisplay.CollapseSection -> {
-//
-//            }
-//        }
+
     }
 
-    class ThumbnailViewHolder(private var binding: ViewBinding) : ArtWorkDetailsViewHolder(binding)
-    class TextViewHolder(private var binding: ViewBinding) : ArtWorkDetailsViewHolder(binding)
-    class SectionViewHolder(private var binding: ViewBinding) : ArtWorkDetailsViewHolder(binding)
+    class TextViewHolder(private var binding: ViewBinding) : ArtWorkDetailsViewHolder(binding) {
+        override fun bind(artWorkDetailDisplay: ArtWorkDetailDisplay) {
+            val bi = binding as LayoutArtworkDetailsTitleViewBinding
+            bi.displayItem = artWorkDetailDisplay as ArtWorkDetailDisplay.Text
+            bi.executePendingBindings()
+        }
+    }
+
+    class SectionViewHolder(private var binding: ViewBinding) : ArtWorkDetailsViewHolder(binding) {
+        override fun bind(artWorkDetailDisplay: ArtWorkDetailDisplay) {
+            val bi = binding as LayoutArtworkDetailsSectionViewBinding
+            bi.displayItem = artWorkDetailDisplay as ArtWorkDetailDisplay.Section
+            bi.executePendingBindings()
+        }
+    }
+
     class CollapseSectionViewHolder(private var binding: ViewBinding) :
-        ArtWorkDetailsViewHolder(binding)
+        ArtWorkDetailsViewHolder(binding) {
+        override fun bind(artWorkDetailDisplay: ArtWorkDetailDisplay) {
+            val bi = binding as LayoutArworkDetailsCollapseSectionViewBinding
+            bi.displayItem = artWorkDetailDisplay as ArtWorkDetailDisplay.CollapseSection
+            bi.executePendingBindings()
+        }
+    }
 
 }
