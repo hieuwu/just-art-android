e51fcca409e15b45aed9027f833b06d1c79981eb
Hieu Vu <star.hieu.99@gmail.com>
2022-07-27 21:59:09 +0700

    Create adapter

--- app/src/main/java/com/hieuwu/justart/presentation/artworkdetails/ArtWorkDetailsAdapter.kt
+++ app/src/main/java/com/hieuwu/justart/presentation/artworkdetails/ArtWorkDetailsAdapter.kt
@@ -1,4 +1,90 @@
 package com.hieuwu.justart.presentation.artworkdetails
 
-class ArtWorkDetailsAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import com.hieuwu.justart.R
+import com.hieuwu.justart.databinding.*
+
+
+class ArtWorkDetailsAdapter :
+    ListAdapter<ArtWorkDetailDisplay, ArtWorkDetailsViewHolder>(DiffCallback) {
+    override fun getItemViewType(position: Int): Int {
+        return when (getItem(position)) {
+            is ArtWorkDetailDisplay.Thumbnail -> {
+                R.layout.layout_artwork_details_title_view
+            }
+            is ArtWorkDetailDisplay.Text -> {
+                R.layout.layout_artwork_details_thumbnail_view
+            }
+            is ArtWorkDetailDisplay.Section -> {
+                R.layout.layout_artwork_details_section_view
+            }
+            is ArtWorkDetailDisplay.CollapseSection -> {
+                R.layout.custom_collapse_paragraph_view
+            }
+            else -> {
+                super.getItemViewType(position)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArtWorkDetailsViewHolder {
+        return when (viewType) {
+            R.layout.layout_artwork_details_title_view -> {
+                ArtWorkDetailsViewHolder.TextViewHolder(
+                    LayoutArtworkDetailsTitleViewBinding
+                        .inflate(LayoutInflater.from(parent.context))
+                )
+            }
+
+            R.layout.layout_artwork_details_thumbnail_view -> {
+                ArtWorkDetailsViewHolder.ThumbnailViewHolder(
+                    LayoutArtworkDetailsThumbnailViewBinding
+                        .inflate(LayoutInflater.from(parent.context))
+                )
+            }
+
+            R.layout.layout_artwork_details_section_view -> {
+                ArtWorkDetailsViewHolder.SectionViewHolder(
+                    LayoutArtworkDetailsSectionViewBinding
+                        .inflate(LayoutInflater.from(parent.context))
+                )
+            }
+
+            R.layout.custom_collapse_paragraph_view -> {
+                ArtWorkDetailsViewHolder.CollapseSectionViewHolder(
+                    LayoutArworkDetailsCollapseSectionViewBinding
+                        .inflate(LayoutInflater.from(parent.context))
+                )
+            }
+            else -> {
+                return ArtWorkDetailsViewHolder(
+                    LayoutArtWorksItemBinding.inflate(LayoutInflater.from(parent.context))
+                )
+            }
+        }
+    }
+
+    override fun onBindViewHolder(holder: ArtWorkDetailsViewHolder, position: Int) {
+        val artWork = getItem(position)
+
+    }
+
+    companion object DiffCallback : DiffUtil.ItemCallback<ArtWorkDetailDisplay>() {
+        override fun areItemsTheSame(
+            oldItem: ArtWorkDetailDisplay,
+            newItem: ArtWorkDetailDisplay
+        ): Boolean {
+            return true
+        }
+
+        override fun areContentsTheSame(
+            oldItem: ArtWorkDetailDisplay,
+            newItem: ArtWorkDetailDisplay
+        ): Boolean {
+            return true
+        }
+    }
 }
