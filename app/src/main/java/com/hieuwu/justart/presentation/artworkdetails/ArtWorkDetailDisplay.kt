be31448e2013276a2b5806c5f8290183e767f2d7
Hieu Vu <star.hieu.99@gmail.com>
2022-07-27 23:01:05 +0700

    Add item to list

--- app/src/main/java/com/hieuwu/justart/presentation/artworkdetails/ArtWorkDetailDisplay.kt
+++ app/src/main/java/com/hieuwu/justart/presentation/artworkdetails/ArtWorkDetailDisplay.kt
@@ -12,83 +12,105 @@ open class ArtWorkDetailDisplay {
 
 fun mapToDisplay(artWorkDetailsDo: ArtWorkDetailsDo?): List<ArtWorkDetailDisplay> {
     val displayList = mutableListOf<ArtWorkDetailDisplay>()
-    artWorkDetailsDo?.imageUrl?.let { ArtWorkDetailDisplay.Thumbnail(imageUrl = it) }
-    artWorkDetailsDo?.title?.let { ArtWorkDetailDisplay.Text(text = it) }
-    artWorkDetailsDo?.dateDisplay?.let { ArtWorkDetailDisplay.Text(text = it) }
-    artWorkDetailsDo?.artistDisplay?.let { ArtWorkDetailDisplay.Text(text = it) }
+    artWorkDetailsDo?.imageUrl?.let { displayList.add(ArtWorkDetailDisplay.Thumbnail(imageUrl = it)) }
+    artWorkDetailsDo?.title?.let { displayList.add(ArtWorkDetailDisplay.Text(text = it)) }
+    artWorkDetailsDo?.dateDisplay?.let { displayList.add(ArtWorkDetailDisplay.Text(text = it)) }
+    artWorkDetailsDo?.artistDisplay?.let { displayList.add(ArtWorkDetailDisplay.Text(text = it)) }
     artWorkDetailsDo?.artistTitle?.let {
-        ArtWorkDetailDisplay.Section(
-            title = "Artist",
-            content = it,
+        displayList.add(
+            ArtWorkDetailDisplay.Section(
+                title = "Artist",
+                content = it,
+            )
         )
     }
     artWorkDetailsDo?.title?.let {
-        ArtWorkDetailDisplay.Section(
-            title = "Title",
-            content = it,
+        displayList.add(
+            ArtWorkDetailDisplay.Section(
+                title = "Title",
+                content = it,
+            )
         )
     }
 
     artWorkDetailsDo?.placeOfOrigin?.let {
-        ArtWorkDetailDisplay.Section(
-            title = "Origin",
-            content = it,
+        displayList.add(
+            ArtWorkDetailDisplay.Section(
+                title = "Origin",
+                content = it,
+            )
         )
     }
 
     artWorkDetailsDo?.dateDisplay?.let {
-        ArtWorkDetailDisplay.Section(
-            title = "Date",
-            content = it,
+        displayList.add(
+            ArtWorkDetailDisplay.Section(
+                title = "Date",
+                content = it,
+            )
         )
     }
 
     artWorkDetailsDo?.mediumDisplay?.let {
-        ArtWorkDetailDisplay.Section(
-            title = "Medium",
-            content = it,
+        displayList.add(
+            ArtWorkDetailDisplay.Section(
+                title = "Medium",
+                content = it,
+            )
         )
     }
 
     artWorkDetailsDo?.dimensions?.let {
-        ArtWorkDetailDisplay.Section(
-            title = "Dimensions",
-            content = it,
+        displayList.add(
+            ArtWorkDetailDisplay.Section(
+                title = "Dimensions",
+                content = it,
+            )
         )
     }
 
     artWorkDetailsDo?.creditLine?.let {
-        ArtWorkDetailDisplay.Section(
-            title = "Credit Line",
-            content = it,
+        displayList.add(
+            ArtWorkDetailDisplay.Section(
+                title = "Credit Line",
+                content = it,
+            )
         )
     }
 
     artWorkDetailsDo?.mainReferenceNumber?.let {
-        ArtWorkDetailDisplay.Section(
-            title = "Reference Number",
-            content = it,
+        displayList.add(
+            ArtWorkDetailDisplay.Section(
+                title = "Reference Number",
+                content = it,
+            )
         )
     }
 
     artWorkDetailsDo?.publicationHistory?.let {
-        ArtWorkDetailDisplay.CollapseSection(
-            title = "Publication history",
-            content = it,
+        displayList.add(
+            ArtWorkDetailDisplay.CollapseSection(
+                title = "Publication history",
+                content = it,
+            )
         )
     }
 
     artWorkDetailsDo?.exhibitionHistory?.let {
-        ArtWorkDetailDisplay.CollapseSection(
-            title = "Exhibition history",
-            content = it,
+        displayList.add(
+            ArtWorkDetailDisplay.CollapseSection(
+                title = "Exhibition history",
+                content = it,
+            )
         )
     }
 
     artWorkDetailsDo?.provenanceText?.let {
-        ArtWorkDetailDisplay.CollapseSection(
-            title = "Provenance",
-            content = it,
+        displayList.add(
+            ArtWorkDetailDisplay.CollapseSection(
+                title = "Provenance",
+                content = it,
+            )
         )
     }
 
