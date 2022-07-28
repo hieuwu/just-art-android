24e2b054a98b20225c437c7b15e8d09ffad6086a
unknown <dohonghuan@gmail.com>
2022-07-28 15:10:25 +0700

    Add ArtWorkDo. Mapping from ArtWork to ArtWorkDo

---
+++ app/src/main/java/com/hieuwu/justart/mapper/ArtWorkMapper.kt
@@ -0,0 +1,31 @@
+package com.hieuwu.justart.mapper
+
+import com.hieuwu.justart.domain.models.ArtWorkDo
+import com.hieuwu.justartsdk.artworks.v1.domain.ArtWork
+
+fun List<ArtWork>.asDomainModel(): List<ArtWorkDo> {
+    return map {
+        ArtWorkDo(
+            id = it.id,
+            title = it.title,
+            thumbnail = it.thumbnail?.asDomainModel(),
+            artistDisplay = it.artistDisplay,
+            dimensions = it.dimensions,
+            mediumDisplay = it.mediumDisplay,
+            inscriptions = it.inscriptions,
+            creditLine = it.creditLine,
+            placeOfOrigin = it.placeOfOrigin,
+            publicationHistory = it.publicationHistory,
+            exhibitionHistory = it.exhibitionHistory,
+            provenanceText = it.provenanceText,
+            imageUrl = it.imageUrl
+        )
+    }
+}
+
+fun ArtWork.Thumbnail.asDomainModel() = ArtWorkDo.Thumbnail(
+    imageUrlBase64 = this.imageUrlBase64,
+    width = this.width,
+    height = this.height,
+    altText = this.altText
+)
