87e9a4c9a3bea63c874fd1efdc11b99ba9dc4543
Hieu Vu <star.hieu.99@gmail.com>
2022-07-29 23:42:22 +0700

    Create animation from list

--- app/src/main/java/com/hieuwu/justart/presentation/artworks/ArtWorksFragment.kt
+++ app/src/main/java/com/hieuwu/justart/presentation/artworks/ArtWorksFragment.kt
@@ -1,17 +1,23 @@
 package com.hieuwu.justart.presentation.artworks
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.hieuwu.justart.R
 import com.hieuwu.justart.databinding.FragmentArtWorksBinding
 import com.hieuwu.justart.domain.usecases.RetrieveArtWorksUseCase
+import com.hieuwu.justart.presentation.views.SpaceDecoration
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -32,6 +38,7 @@ class ArtWorksFragment : Fragment() {
         return binding.root
     }
 
+    private var recyclerviewAdapter: ArtWorksAdapter? = null
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val viewModelFactory = ArtWorksViewModelFactory(retrieveArtWorksUseCase)
@@ -41,6 +48,36 @@ class ArtWorksFragment : Fragment() {
 
         setObservers()
         setupRecyclerView(binding.artWorksRecyclerView)
+        if (savedInstanceState != null) {
+            recyclerviewAdapter?.restoreInstanceState(savedInstanceState)
+        }
+        if (recyclerviewAdapter!!.expectsTransition) {
+            // We are transitioning back from CheeseDetailFragment.
+            // Postpone the transition animation until the destination item is ready.
+            postponeEnterTransition(500L, TimeUnit.MILLISECONDS)
+        }
+
+//        val toolbar: Toolbar = view.findViewById(R.id.toolbar)
+        val grid: RecyclerView = binding.artWorksRecyclerView
+
+        // Adjust the edge-to-edge display.
+        val gridPadding = resources.getDimensionPixelSize(R.dimen.spacing_tiny)
+        ViewCompat.setOnApplyWindowInsetsListener(view.parent as View) { _, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            toolbar.updateLayoutParams<AppBarLayout.LayoutParams> {
+//                topMargin = systemBars.top
+//            }
+            grid.updatePadding(
+                left = gridPadding + systemBars.left,
+                right = gridPadding + systemBars.right,
+                bottom = gridPadding + systemBars.bottom
+            )
+            insets
+        }
+
+        grid.addItemDecoration(
+            SpaceDecoration(resources.getDimensionPixelSize(R.dimen.spacing_tiny))
+        )
     }
 
     private fun setObservers() {
@@ -53,18 +90,23 @@ class ArtWorksFragment : Fragment() {
     }
 
     private fun setupRecyclerView(recyclerView: RecyclerView) {
+        recyclerviewAdapter = ArtWorksAdapter(
+            ArtWorksAdapter.OnClickListener(
+                clickListener = { viewModel.displayPropertyDetails(it) },
+            ),
+            onReadyToTransition = { startPostponedEnterTransition() }
+        )
+
         with(recyclerView) {
             layoutManager = GridLayoutManager(context, 2)
-            adapter = ArtWorksAdapter(
-                ArtWorksAdapter.OnClickListener(
-                    clickListener = { viewModel.displayPropertyDetails(it) }
-                )
-            )
+            adapter = recyclerviewAdapter
         }
     }
 
     private fun navigateToArtWorksDetail(id: Int) {
-        val direction = ArtWorksFragmentDirections.actionArtWorksFragmentToArtDetailsFragment(id = id)
+        val direction =
+            ArtWorksFragmentDirections.actionArtWorksFragmentToArtDetailsFragment(id = id)
         findNavController().navigate(direction)
     }
+
 }
