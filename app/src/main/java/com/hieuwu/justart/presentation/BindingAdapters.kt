1a3b8407b620c4add807d9c3361ed492394c1fca
Hieu Vu <star.hieu.99@gmail.com>
2022-07-29 21:30:35 +0700

    Fix build error

--- app/src/main/java/com/hieuwu/justart/presentation/BindingAdapters.kt
+++ app/src/main/java/com/hieuwu/justart/presentation/BindingAdapters.kt
@@ -7,7 +7,6 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.hieuwu.justart.R
-import com.hieuwu.justart.domain.models.ArtWork
 import com.hieuwu.justart.presentation.artworkdetails.ArtWorkDetailDisplay
 import com.hieuwu.justart.presentation.artworkdetails.ArtWorkDetailsAdapter
 import com.hieuwu.justart.domain.models.ArtWorkDo
