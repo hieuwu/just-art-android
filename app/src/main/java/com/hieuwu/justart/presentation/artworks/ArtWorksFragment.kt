9f668d7c8bf3543de6dc5011895e546d1bc84063
Hieu Vu <star.hieu.99@gmail.com>
2022-07-30 15:38:42 +0700

    Handle animation in list

--- app/src/main/java/com/hieuwu/justart/presentation/artworks/ArtWorksFragment.kt
+++ app/src/main/java/com/hieuwu/justart/presentation/artworks/ArtWorksFragment.kt
@@ -1,21 +1,27 @@
 package com.hieuwu.justart.presentation.artworks
 
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
 import androidx.core.view.updatePadding
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.transition.Explode
+import androidx.transition.Slide
+import com.google.android.material.appbar.AppBarLayout
 import com.hieuwu.justart.R
 import com.hieuwu.justart.databinding.FragmentArtWorksBinding
 import com.hieuwu.justart.domain.usecases.RetrieveArtWorksUseCase
-import com.hieuwu.justart.presentation.views.SpaceDecoration
+import com.hieuwu.justart.presentation.views.*
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
@@ -30,6 +36,48 @@ class ArtWorksFragment : Fragment() {
 
     private lateinit var viewModel: ArtWorksViewModel
 
+    private var recyclerviewAdapter: ArtWorksAdapter? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // This is the transition to be used for non-shared elements when we are opening the detail
+        // screen.
+        exitTransition = transitionTogether {
+            duration = LARGE_EXPAND_DURATION / 2
+            interpolator = FAST_OUT_LINEAR_IN
+            // The app bar.
+            this += Slide(Gravity.TOP).apply {
+                mode = Slide.MODE_OUT
+                addTarget(R.id.app_bar)
+            }
+            // The grid items.
+            this += Explode().apply {
+                mode = Explode.MODE_OUT
+                excludeTarget(R.id.app_bar, true)
+            }
+        }
+
+        // This is the transition to be used for non-shared elements when we are return back from
+        // the detail screen.
+        reenterTransition = transitionTogether {
+            duration = LARGE_COLLAPSE_DURATION / 2
+            interpolator = LINEAR_OUT_SLOW_IN
+            // The app bar.
+            this += Slide(Gravity.TOP).apply {
+                mode = Slide.MODE_IN
+                addTarget(R.id.app_bar)
+            }
+            // The grid items.
+            this += Explode().apply {
+                // The grid items should start imploding after the app bar is in.
+                startDelay = LARGE_COLLAPSE_DURATION / 2
+                mode = Explode.MODE_IN
+                excludeTarget(R.id.app_bar, true)
+            }
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -38,14 +86,12 @@ class ArtWorksFragment : Fragment() {
         return binding.root
     }
 
-    private var recyclerviewAdapter: ArtWorksAdapter? = null
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val viewModelFactory = ArtWorksViewModelFactory(retrieveArtWorksUseCase)
         viewModel = ViewModelProvider(this, viewModelFactory)[ArtWorksViewModel::class.java]
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
-
         setObservers()
         setupRecyclerView(binding.artWorksRecyclerView)
         if (savedInstanceState != null) {
@@ -64,9 +110,9 @@ class ArtWorksFragment : Fragment() {
         val gridPadding = resources.getDimensionPixelSize(R.dimen.spacing_tiny)
         ViewCompat.setOnApplyWindowInsetsListener(view.parent as View) { _, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            toolbar.updateLayoutParams<AppBarLayout.LayoutParams> {
-//                topMargin = systemBars.top
-//            }
+            binding.toolbar.updateLayoutParams<AppBarLayout.LayoutParams> {
+                topMargin = systemBars.top
+            }
             grid.updatePadding(
                 left = gridPadding + systemBars.left,
                 right = gridPadding + systemBars.right,
@@ -78,6 +124,7 @@ class ArtWorksFragment : Fragment() {
         grid.addItemDecoration(
             SpaceDecoration(resources.getDimensionPixelSize(R.dimen.spacing_tiny))
         )
+//        grid.adapter = recyclerviewAdapter
     }
 
     private fun setObservers() {
@@ -96,9 +143,7 @@ class ArtWorksFragment : Fragment() {
             ),
             onReadyToTransition = { startPostponedEnterTransition() }
         )
-
         with(recyclerView) {
-            layoutManager = GridLayoutManager(context, 2)
             adapter = recyclerviewAdapter
         }
     }
@@ -109,4 +154,10 @@ class ArtWorksFragment : Fragment() {
         findNavController().navigate(direction)
     }
 
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        recyclerviewAdapter?.saveInstanceState(outState)
+        super.onSaveInstanceState(outState)
+    }
+//
 }
