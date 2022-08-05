2a9d3468dad5d24fb18f31b58d5a6567e4f20c1f
Hieu Vu <star.hieu.99@gmail.com>
2022-08-05 20:35:09 +0700

    Remove unused imports

--- app/src/main/java/com/hieuwu/justart/presentation/artworkdetails/ArtWorkDetailsViewModel.kt
+++ app/src/main/java/com/hieuwu/justart/presentation/artworkdetails/ArtWorkDetailsViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.hieuwu.justart.domain.models.ArtWorkDetailsDo
 import com.hieuwu.justart.domain.usecases.RetrieveArtWorkDetailsUseCase
 import kotlinx.coroutines.launch
 import javax.inject.Inject
