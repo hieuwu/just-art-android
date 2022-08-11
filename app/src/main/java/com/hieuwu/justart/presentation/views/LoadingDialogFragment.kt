5a4af768668f2c454bd299bb7d911f316340c4e1
Hieu Vu <star.hieu.99@gmail.com>
2022-08-11 22:10:26 +0700

    Create loading dialog fragment

--- app/src/main/java/com/hieuwu/justart/presentation/views/LoadingDialogFragment.kt
+++ app/src/main/java/com/hieuwu/justart/presentation/views/LoadingDialogFragment.kt
@@ -1,4 +1,21 @@
 package com.hieuwu.justart.presentation.views
 
-class LoadingDialogFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.hieuwu.justart.R
+
+class LoadingDialogFragment: DialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        isCancelable = false
+        return inflater.inflate(R.layout.layout_loading_dialog, container, false)
+    }
+
 }
+
