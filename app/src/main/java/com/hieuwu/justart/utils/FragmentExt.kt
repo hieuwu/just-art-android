a9a6c6d049616a95d718bba59d6216138bfe7d45
Hieu Vu <star.hieu.99@gmail.com>
2022-08-11 22:39:21 +0700

    Implement extensions

--- app/src/main/java/com/hieuwu/justart/utils/FragmentExt.kt
+++ app/src/main/java/com/hieuwu/justart/utils/FragmentExt.kt
@@ -1,4 +1,14 @@
 package com.hieuwu.justart.utils
 
-class FragmentExt {
+import androidx.fragment.app.Fragment
+import com.hieuwu.justart.presentation.views.LoadingDialogFragment
+
+const val TAG = "LOADING_DIALOG_TAG"
+
+fun Fragment.showLoading() {
+    LoadingDialogFragment.getInstance().show(parentFragmentManager, TAG)
+}
+
+fun Fragment.hideLoading() {
+    LoadingDialogFragment.getInstance().dismiss()
 }
