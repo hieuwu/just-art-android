e95287cc60dfe8dc8b643c2f42df69c8e92d353a
Hieu Vu <star.hieu.99@gmail.com>
2022-08-13 20:59:11 +0700

    Remove unused properties

--- app/src/main/java/com/hieuwu/justart/presentation/views/LoadingDialogFragment.kt
+++ app/src/main/java/com/hieuwu/justart/presentation/views/LoadingDialogFragment.kt
@@ -1,9 +1,12 @@
 package com.hieuwu.justart.presentation.views
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.fragment.app.DialogFragment
 import com.hieuwu.justart.R
 
@@ -31,6 +34,10 @@ class LoadingDialogFragment : DialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         isCancelable = false
+        dialog?.window?.apply {
+            setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
+        }
         return inflater.inflate(R.layout.layout_loading_dialog, container, false)
     }
 }
