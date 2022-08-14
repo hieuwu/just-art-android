023d9c42951c200d03547ceef28c8fe5d8cbe966
Hieu Vu <star.hieu.99@gmail.com>
2022-08-14 22:48:36 +0700

    Add error view

--- app/src/main/java/com/hieuwu/justart/presentation/views/custom/ErrorView.kt
+++ app/src/main/java/com/hieuwu/justart/presentation/views/custom/ErrorView.kt
@@ -1,4 +1,30 @@
-package com.hieuwu.justart.presentation.views
+package com.hieuwu.justart.presentation.views.custom
 
-class ErrorView {
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+import com.hieuwu.justart.R
+
+class ErrorView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null
+) : LinearLayout(context, attrs) {
+    init {
+        val view = inflate(
+            context,
+            R.layout.layout_error_view, this
+        )
+
+//        titleTextView = view.findViewById(R.id.titleText)
+//        contentTextView = view.findViewById(R.id.paragraphText)
+//        titleLayout = view.findViewById(R.id.titleLayout)
+//        arrowButton = view.findViewById(R.id.arrowButton)
+//
+//        context.withStyledAttributes(attrs, R.styleable.CollapseParagraphView) {
+//            titleTextView?.text = getString(R.styleable.CollapseParagraphView_titleValue)
+//            contentTextView?.text = getString(R.styleable.CollapseParagraphView_contentValue)
+//            showChevron = getBoolean(R.styleable.CollapseParagraphView_showChevron, false)
+//        }
+//        populateChevron(showChevron)
+    }
+
 }
