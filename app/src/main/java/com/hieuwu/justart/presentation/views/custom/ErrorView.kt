aadaccc1e9c6ba57bd6b967381c98054d066c707
Hieu Vu <star.hieu.99@gmail.com>
2022-08-16 22:47:26 +0700

    Remove unused code

--- app/src/main/java/com/hieuwu/justart/presentation/views/custom/ErrorView.kt
+++ app/src/main/java/com/hieuwu/justart/presentation/views/custom/ErrorView.kt
@@ -9,22 +9,10 @@ class ErrorView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null
 ) : LinearLayout(context, attrs) {
     init {
-        val view = inflate(
+        inflate(
             context,
             R.layout.layout_error_view, this
         )
-
-//        titleTextView = view.findViewById(R.id.titleText)
-//        contentTextView = view.findViewById(R.id.paragraphText)
-//        titleLayout = view.findViewById(R.id.titleLayout)
-//        arrowButton = view.findViewById(R.id.arrowButton)
-//
-//        context.withStyledAttributes(attrs, R.styleable.CollapseParagraphView) {
-//            titleTextView?.text = getString(R.styleable.CollapseParagraphView_titleValue)
-//            contentTextView?.text = getString(R.styleable.CollapseParagraphView_contentValue)
-//            showChevron = getBoolean(R.styleable.CollapseParagraphView_showChevron, false)
-//        }
-//        populateChevron(showChevron)
     }
 
 }
