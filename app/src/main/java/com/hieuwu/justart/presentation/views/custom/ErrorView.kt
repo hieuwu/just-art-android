5976e3ffd9ca047dc1be31dc599facdfcd71caac
Hieu Vu <star.hieu.99@gmail.com>
2022-08-18 22:51:02 +0700

    Setup callback for refreshing

--- app/src/main/java/com/hieuwu/justart/presentation/views/custom/ErrorView.kt
+++ app/src/main/java/com/hieuwu/justart/presentation/views/custom/ErrorView.kt
@@ -2,17 +2,29 @@ package com.hieuwu.justart.presentation.views.custom
 
 import android.content.Context
 import android.util.AttributeSet
+import android.widget.Button
 import android.widget.LinearLayout
 import com.hieuwu.justart.R
+import com.hieuwu.justart.domain.models.ArtWorkDo
 
 class ErrorView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null
 ) : LinearLayout(context, attrs) {
+    var refreshButton: Button
+    lateinit var refreshClick: () -> Unit
+
     init {
-        inflate(
+        val view = inflate(
             context,
             R.layout.layout_error_view, this
         )
+        refreshButton = view.findViewById(R.id.refresh_button)
+        refreshButton.setOnClickListener {
+            refreshClick()
+        }
     }
 
+    fun setRefresh(onRefresh: () -> Unit) {
+        refreshClick = onRefresh
+    }
 }
