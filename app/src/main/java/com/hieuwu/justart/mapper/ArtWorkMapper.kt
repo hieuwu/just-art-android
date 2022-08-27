792411c2b09ee0001742fefb704add806b24ee00
Hieu Vu <star.hieu.99@gmail.com>
2022-08-27 12:59:09 +0700

    Remove unused properties

--- app/src/main/java/com/hieuwu/justart/mapper/ArtWorkMapper.kt
+++ app/src/main/java/com/hieuwu/justart/mapper/ArtWorkMapper.kt
@@ -8,24 +8,8 @@ fun List<ArtWork>.asDomainModel(): List<ArtWorkDo> {
         ArtWorkDo(
             id = it.id,
             title = it.title,
-            thumbnail = it.thumbnail?.asDomainModel(),
-            artistDisplay = it.artistDisplay,
-            dimensions = it.dimensions,
-            mediumDisplay = it.mediumDisplay,
-            inscriptions = it.inscriptions,
-            creditLine = it.creditLine,
-            placeOfOrigin = it.placeOfOrigin,
-            publicationHistory = it.publicationHistory,
-            exhibitionHistory = it.exhibitionHistory,
-            provenanceText = it.provenanceText,
-            imageUrl = it.imageUrl
+            imageUrl = it.imageUrl,
+            artistDisplay = it.artistDisplay
         )
     }
 }
-
-fun ArtWork.Thumbnail.asDomainModel() = ArtWorkDo.Thumbnail(
-    imageUrlBase64 = this.imageUrlBase64,
-    width = this.width,
-    height = this.height,
-    altText = this.altText
-)
