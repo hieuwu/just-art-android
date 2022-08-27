bbf11a6b6bab2aff7e32a706afddd8cd9731139f
Hieu Vu <star.hieu.99@gmail.com>
2022-08-27 12:59:15 +0700

    Remove unused properties

--- app/src/main/java/com/hieuwu/justart/mapper/ArtWorkDetailsMapper.kt
+++ app/src/main/java/com/hieuwu/justart/mapper/ArtWorkDetailsMapper.kt
@@ -5,15 +5,10 @@ import com.hieuwu.justartsdk.artworks.v1.domain.ArtWorkDetails
 
 fun ArtWorkDetails.asDomainModel() = ArtWorkDetailsDo(
     id = this.id,
-    thumbnail = this.thumbnail?.asDomainModel(),
     dateDisplay = this.dateDisplay,
     artistDisplay = this.artistDisplay,
     publicationHistory = this.publicationHistory,
-    isOnView = this.isOnView,
-    galleryTitle = this.galleryTitle,
-    artistTitle = this.artistTitle,
     title = this.title,
-    departmentTitle = this.departmentTitle,
     placeOfOrigin = this.placeOfOrigin,
     mediumDisplay = this.mediumDisplay,
     dimensions = this.dimensions,
@@ -24,19 +19,3 @@ fun ArtWorkDetails.asDomainModel() = ArtWorkDetailsDo(
     imageUrl = this.imageUrl
 )
 
-
-fun ArtWorkDetails.Color.asDomainModel() = ArtWorkDetailsDo.Color(
-    h = this.h,
-    l = this.h,
-    s = this.s,
-    percentage = this.percentage,
-    population = this.population
-
-)
-
-fun ArtWorkDetails.Thumbnail.asDomainModel() = ArtWorkDetailsDo.Thumbnail(
-    lqip = this.lqip,
-    width = this.width,
-    height = this.height,
-    altText = this.altText
-)
