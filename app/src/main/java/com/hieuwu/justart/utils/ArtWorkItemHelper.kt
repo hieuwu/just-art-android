76050e34580d0f7a90af9babe653c9bbd94e5a03
Hieu Vu <star.hieu.99@gmail.com>
2022-09-05 19:42:57 +0700

    Create factory for item helper

--- app/src/main/java/com/hieuwu/justart/utils/ArtWorkItemHelper.kt
+++ app/src/main/java/com/hieuwu/justart/utils/ArtWorkItemHelper.kt
@@ -1,7 +1,18 @@
 package com.hieuwu.justart.utils
 
+import android.content.Context
+import android.view.View
+import com.hieuwu.justart.domain.models.ArtWorkDo
+
 interface ArtWorkItemHelper {
-    fun shareArtWork()
+    fun shareArtWork(artwork: ArtWorkDo)
     fun favouriteArtWork()
-    fun clickArtWork()
+    fun clickArtWork(itemView: View, title: View, image: View, card: View,
+                     navArg: Int)
+}
+
+object ArtWorkItemHelperFactory {
+    fun create(context: Context): ArtWorkItemHelper {
+        return ArtWorkItemHelperImpl(context)
+    }
 }
