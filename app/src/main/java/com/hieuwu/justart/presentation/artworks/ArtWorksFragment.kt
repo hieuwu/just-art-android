a732490091f2046ce45f73a28f16a1bad0290f8a
Hieu Vu <star.hieu.99@gmail.com>
2022-09-05 19:55:50 +0700

    Separate logic of transition

--- app/src/main/java/com/hieuwu/justart/presentation/artworks/ArtWorksFragment.kt
+++ app/src/main/java/com/hieuwu/justart/presentation/artworks/ArtWorksFragment.kt
@@ -1,7 +1,6 @@
 package com.hieuwu.justart.presentation.artworks
 
 import android.os.Bundle
-import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,23 +13,12 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
-import androidx.transition.Explode
-import androidx.transition.Slide
 import com.google.android.material.appbar.AppBarLayout
 import com.hieuwu.justart.R
 import com.hieuwu.justart.databinding.FragmentArtWorksBinding
 import com.hieuwu.justart.domain.usecases.RetrieveArtWorksUseCase
-import com.hieuwu.justart.presentation.views.FAST_OUT_LINEAR_IN
-import com.hieuwu.justart.presentation.views.LARGE_COLLAPSE_DURATION
-import com.hieuwu.justart.presentation.views.LARGE_EXPAND_DURATION
-import com.hieuwu.justart.presentation.views.LINEAR_OUT_SLOW_IN
 import com.hieuwu.justart.presentation.views.animation.helper.SpaceDecoration
-import com.hieuwu.justart.presentation.views.animation.helper.plusAssign
-import com.hieuwu.justart.presentation.views.animation.helper.transitionTogether
-import com.hieuwu.justart.utils.ArtWorkItemHelper
-import com.hieuwu.justart.utils.ArtWorkItemHelperFactory
-import com.hieuwu.justart.utils.hideLoading
-import com.hieuwu.justart.utils.showLoading
+import com.hieuwu.justart.utils.*
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
 import java.util.concurrent.TimeUnit
@@ -55,42 +43,6 @@ class ArtWorksFragment : Fragment() {
         super.onCreate(savedInstanceState)
         setupExitTransition()
         setupReEnterTransition()
-    }
-
-    private fun setupExitTransition() {
-        exitTransition = transitionTogether {
-            duration = LARGE_EXPAND_DURATION / 2
-            interpolator = FAST_OUT_LINEAR_IN
-            // The app bar.
-            this += Slide(Gravity.TOP).apply {
-                mode = Slide.MODE_OUT
-                addTarget(R.id.app_bar)
-            }
-            // The grid items.
-            this += Explode().apply {
-                mode = Explode.MODE_OUT
-                excludeTarget(R.id.app_bar, true)
-            }
-        }
-    }
-
-    private fun setupReEnterTransition() {
-        reenterTransition = transitionTogether {
-            duration = LARGE_COLLAPSE_DURATION / 2
-            interpolator = LINEAR_OUT_SLOW_IN
-            // The app bar.
-            this += Slide(Gravity.TOP).apply {
-                mode = Slide.MODE_IN
-                addTarget(R.id.app_bar)
-            }
-            // The grid items.
-            this += Explode().apply {
-                // The grid items should start imploding after the app bar is in.
-                startDelay = LARGE_COLLAPSE_DURATION / 2
-                mode = Explode.MODE_IN
-                excludeTarget(R.id.app_bar, true)
-            }
-        }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
--- app/src/main/java/com/hieuwu/justart/utils/FragmentExt.kt
+++ app/src/main/java/com/hieuwu/justart/utils/FragmentExt.kt
@@ -1,6 +1,16 @@
 package com.hieuwu.justart.utils
 
+import android.view.Gravity
 import androidx.fragment.app.Fragment
+import androidx.transition.Explode
+import androidx.transition.Slide
+import com.hieuwu.justart.R
+import com.hieuwu.justart.presentation.views.FAST_OUT_LINEAR_IN
+import com.hieuwu.justart.presentation.views.LARGE_COLLAPSE_DURATION
+import com.hieuwu.justart.presentation.views.LARGE_EXPAND_DURATION
+import com.hieuwu.justart.presentation.views.LINEAR_OUT_SLOW_IN
+import com.hieuwu.justart.presentation.views.animation.helper.plusAssign
+import com.hieuwu.justart.presentation.views.animation.helper.transitionTogether
 import com.hieuwu.justart.presentation.views.custom.LoadingDialogFragment
 
 const val TAG = "LOADING_DIALOG_TAG"
@@ -11,4 +21,40 @@ fun Fragment.showLoading() {
 
 fun Fragment.hideLoading() {
     LoadingDialogFragment.getInstance().dismiss()
+}
+
+fun Fragment.setupExitTransition() {
+    exitTransition = transitionTogether {
+        duration = LARGE_EXPAND_DURATION / 2
+        interpolator = FAST_OUT_LINEAR_IN
+        // The app bar.
+        this += Slide(Gravity.TOP).apply {
+            mode = Slide.MODE_OUT
+            addTarget(R.id.app_bar)
+        }
+        // The grid items.
+        this += Explode().apply {
+            mode = Explode.MODE_OUT
+            excludeTarget(R.id.app_bar, true)
+        }
+    }
+}
+
+fun Fragment.setupReEnterTransition() {
+    reenterTransition = transitionTogether {
+        duration = LARGE_COLLAPSE_DURATION / 2
+        interpolator = LINEAR_OUT_SLOW_IN
+        // The app bar.
+        this += Slide(Gravity.TOP).apply {
+            mode = Slide.MODE_IN
+            addTarget(R.id.app_bar)
+        }
+        // The grid items.
+        this += Explode().apply {
+            // The grid items should start imploding after the app bar is in.
+            startDelay = LARGE_COLLAPSE_DURATION / 2
+            mode = Explode.MODE_IN
+            excludeTarget(R.id.app_bar, true)
+        }
+    }
 }
