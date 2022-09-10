0f1bf7021cb85bbcf087594736efdb6d27d000ea
Hieu Vu <star.hieu.99@gmail.com>
2022-09-10 20:07:43 +0700

    Add use case

--- app/src/main/java/com/hieuwu/justart/domain/usecases/SaveFavoriteArtWorkUseCase.kt
+++ app/src/main/java/com/hieuwu/justart/domain/usecases/SaveFavoriteArtWorkUseCase.kt
@@ -1,4 +1,12 @@
-package com.hieuwu.justart.domain.usecases.impl
+package com.hieuwu.justart.domain.usecases
 
-interface SaveFavoriteArtWorkUseCase {
+import com.hieuwu.justart.domain.models.ArtWorkDo
+
+interface SaveFavoriteArtWorkUseCase : UseCase<SaveFavoriteArtWorkUseCase.Input,
+        SaveFavoriteArtWorkUseCase.Result> {
+    data class Input(val artWork: ArtWorkDo)
+    open class Result {
+        class Success : Result()
+        class Failure : Result()
+    }
 }
