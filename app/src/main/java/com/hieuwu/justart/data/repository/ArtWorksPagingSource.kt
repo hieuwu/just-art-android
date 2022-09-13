7ea06147fb1daf6fcfa7b0b7bc5ed35babb63362
Hieu Vu <star.hieu.99@gmail.com>
2022-09-13 23:07:24 +0700

    Get data from use case

--- app/src/main/java/com/hieuwu/justart/data/repository/ArtWorksPagingSource.kt
+++ app/src/main/java/com/hieuwu/justart/data/repository/ArtWorksPagingSource.kt
@@ -3,30 +3,25 @@ package com.hieuwu.justart.data.repository
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.hieuwu.justart.domain.models.ArtWorkDo
-import com.hieuwu.justart.mapper.asDomainModel
-import com.hieuwu.justartsdk.ApiResult
-import com.hieuwu.justartsdk.artworks.v1.ArtWorksService
-import com.hieuwu.justartsdk.artworks.v1.domain.ArtWorksResponse
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import com.hieuwu.justart.domain.usecases.RetrieveArtWorksUseCase
 import javax.inject.Inject
 
 private const val STARTING_KEY = 0
 
-class ArtWorksPagingSource @Inject constructor(private val artWorksService: ArtWorksService) :
+class ArtWorksPagingSource @Inject constructor(private val retrieveArtWorksUseCase: RetrieveArtWorksUseCase) :
     PagingSource<Int, ArtWorkDo>() {
-
-
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, ArtWorkDo> {
         val start = params.key ?: STARTING_KEY
         val range = start.until(start + params.loadSize)
-        val res: ApiResult<ArtWorksResponse>
-        withContext(Dispatchers.IO) {
-            res = artWorksService.getArtWorks(limit = 12, page = 12)
-        }
-        val data = res.response?.artWorks?.asDomainModel()!!
+        val res = retrieveArtWorksUseCase.execute(
+            RetrieveArtWorksUseCase.Input(
+                limit = params.loadSize,
+                start
+            )
+        ) as RetrieveArtWorksUseCase.Result.Success
+
         return LoadResult.Page(
-            data = data,
+            data = res.data ?: listOf(),
             prevKey = when (start) {
                 STARTING_KEY -> null
                 else -> ensureValidKey(key = range.first - params.loadSize)
