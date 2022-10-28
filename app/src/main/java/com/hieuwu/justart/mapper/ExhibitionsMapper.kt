759044535e32b8801fe909c2aac73332e9238fa7
Hieu Vu <star.hieu.99@gmail.com>
2022-10-28 21:28:55 +0700

    Map object

--- app/src/main/java/com/hieuwu/justart/mapper/ExhibitionsMapper.kt
+++ app/src/main/java/com/hieuwu/justart/mapper/ExhibitionsMapper.kt
@@ -1,4 +1,19 @@
 package com.hieuwu.justart.mapper
 
-class ExhibitionsMapper {
-}
+import com.hieuwu.justart.domain.models.ExhibitionsDo
+import com.hieuwu.justartsdk.exhibitions.v1.domain.Exhibition
+
+fun Exhibition.asDomainModel() = ExhibitionsDo(
+    id = this.id,
+    title = this.title,
+    isFeatured = this.isFeatured,
+    shortDescription = this.shortDescription,
+    webUrl = this.webUrl,
+    imageUrl = this.imageUrl,
+    status = this.status,
+    aicStartAt = this.aicStartAt,
+    aicEndAt = this.aicEndAt,
+    galleryTitle = this.galleryTitle,
+    imageId = this.imageId,
+    altImageIds = this.altImageIds,
+)
