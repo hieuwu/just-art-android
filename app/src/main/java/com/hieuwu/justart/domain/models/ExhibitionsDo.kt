4272461abd1a9af1e37e6e57168898d3e473b613
Hieu Vu <star.hieu.99@gmail.com>
2022-10-28 21:28:42 +0700

    Create app do

--- app/src/main/java/com/hieuwu/justart/domain/models/ExhibitionsDo.kt
+++ app/src/main/java/com/hieuwu/justart/domain/models/ExhibitionsDo.kt
@@ -1,3 +1,16 @@
 package com.hieuwu.justart.domain.models
 
-data class ExhibitionsDo()
+data class ExhibitionsDo(
+    val id: Int?,
+    val title: String?,
+    val isFeatured: Boolean?,
+    val shortDescription: String?,
+    val webUrl: String?,
+    val imageUrl: String?,
+    val status: String?,
+    val aicStartAt: String?,
+    val aicEndAt: String?,
+    val galleryTitle: String?,
+    val imageId: String?,
+    val altImageIds: List<String>,
+)
