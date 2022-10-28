3cd554c2b450ce7a6a11ae36999e5c373c15df1a
Hieu Vu <star.hieu.99@gmail.com>
2022-10-28 21:28:49 +0700

    Implement use case

--- app/src/main/java/com/hieuwu/justart/domain/usecases/RetrieveExhibitionsUseCase.kt
+++ app/src/main/java/com/hieuwu/justart/domain/usecases/RetrieveExhibitionsUseCase.kt
@@ -1,4 +1,15 @@
 package com.hieuwu.justart.domain.usecases
 
-interface RetrieveExhibitionsUseCase {
+import com.hieuwu.justart.domain.models.ExhibitionsDo
+
+
+interface RetrieveExhibitionsUseCase :
+    UseCase<RetrieveExhibitionsUseCase.Input, RetrieveExhibitionsUseCase.Result> {
+    class Input
+    sealed class Result {
+        data class Success(val data: List<ExhibitionsDo>?) : Result()
+        data class Failure(val error: Error?) : Result()
+    }
+
+    class Error
 }
--- app/src/main/java/com/hieuwu/justart/domain/usecases/impl/RetrieveExhibitionsUseCaseImpl.kt
+++ app/src/main/java/com/hieuwu/justart/domain/usecases/impl/RetrieveExhibitionsUseCaseImpl.kt
@@ -1,4 +1,29 @@
 package com.hieuwu.justart.domain.usecases.impl
 
-class RetrieveExhibitionsUseCaseImpl {
+import com.hieuwu.justart.domain.usecases.RetrieveExhibitionsUseCase
+import com.hieuwu.justart.mapper.asDomainModel
+import com.hieuwu.justartsdk.ApiResult
+import com.hieuwu.justartsdk.exhibitions.v1.ExhibitionsService
+import com.hieuwu.justartsdk.exhibitions.v1.domain.ExhibitionsResponse
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import timber.log.Timber
+import javax.inject.Inject
+
+class RetrieveExhibitionsUseCaseImpl @Inject constructor(
+    private val exhibitionService: ExhibitionsService
+) : RetrieveExhibitionsUseCase {
+    override suspend fun execute(input: RetrieveExhibitionsUseCase.Input): RetrieveExhibitionsUseCase.Result {
+        val res: ApiResult<ExhibitionsResponse>
+        try {
+            withContext(Dispatchers.IO) {
+                res = exhibitionService.getExhibitions()
+            }
+            return RetrieveExhibitionsUseCase.Result.Success(res.response?.data?.map { it.asDomainModel() })
+
+        } catch (e: Exception) {
+            Timber.e(e.message)
+        }
+        return RetrieveExhibitionsUseCase.Result.Success(null)
+    }
 }
