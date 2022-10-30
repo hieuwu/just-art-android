e8a1f2150441f74b47f702b3f7529ef0470ac25d
Hieu Vu <star.hieu.99@gmail.com>
2022-10-30 20:48:06 +0700

    Declare event do

--- app/src/main/java/com/hieuwu/justart/domain/models/EventsDo.kt
+++ app/src/main/java/com/hieuwu/justart/domain/models/EventsDo.kt
@@ -1,4 +1,28 @@
 package com.hieuwu.justart.domain.models
 
-class EventsDo {
-}
+data class EventDo(
+    val id: Int?,
+    val apiModel: String?,
+    val apiLink: String?,
+    val title: String?,
+    val titleDisplay: String?,
+    val imageUrl: String?,
+    val heroCaption: String?,
+    val shortDescription: String?,
+    val headerDescription: String?,
+    val listDescription: String?,
+    val description: String?,
+    val location: String?,
+    val programTitles: List<String>,
+    val ticketedEventId: Int?,
+    val buyButtonCaption: String?,
+    val isRegistrationRequired: Boolean?,
+    val isMemberExclusive: Boolean?,
+    val isFree: Boolean?,
+    val virtualEventUrl: String?,
+    val startDate: String?,
+    val endDate: String?,
+    val startTime: String?,
+    val endTime: String?,
+    val dateDisplay: String?
+)
