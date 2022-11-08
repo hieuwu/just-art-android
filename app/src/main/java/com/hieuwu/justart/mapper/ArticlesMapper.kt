a3b589ddae2939d63b248101fce55f1bd69df77d
Hieu Vu <star.hieu.99@gmail.com>
2022-11-08 19:16:46 +0700

    Map object

--- app/src/main/java/com/hieuwu/justart/mapper/ArticlesMapper.kt
+++ app/src/main/java/com/hieuwu/justart/mapper/ArticlesMapper.kt
@@ -1,2 +1,11 @@
 package com.hieuwu.justart.mapper
 
+import com.hieuwu.justart.domain.models.ArticleDo
+import com.hieuwu.justartsdk.articles.v1.domain.Article
+
+fun Article.asDomainModel() = ArticleDo(
+    id = this.id,
+    title = this.title,
+    artistDisplay = this.artistDisplay,
+    imageUrl = this.imageUrl
+)
