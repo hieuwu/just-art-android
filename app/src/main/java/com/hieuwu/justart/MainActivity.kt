1742c234308d699fd90d57fa31452d44e6518457
dohonghuan <dohonghuan@gmail.com>
2022-11-09 20:29:47 +0700

    Add splash screen

--- app/src/main/java/com/hieuwu/justart/MainActivity.kt
+++ app/src/main/java/com/hieuwu/justart/MainActivity.kt
@@ -2,6 +2,7 @@ package com.hieuwu.justart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -10,6 +11,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navHostFragment = supportFragmentManager.findFragmentById(
