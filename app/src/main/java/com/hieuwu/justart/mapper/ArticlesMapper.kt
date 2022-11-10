be5f60f57c3cd4e2fc70343d4bcef596d02b51de
Hieu Vu <star.hieu.99@gmail.com>
2022-11-10 22:14:26 +0700

    Remove unused properties

--- app/src/main/java/com/hieuwu/justart/mapper/ArticlesMapper.kt
+++ app/src/main/java/com/hieuwu/justart/mapper/ArticlesMapper.kt
@@ -6,7 +6,5 @@ import com.hieuwu.justartsdk.articles.v1.domain.Article
 fun Article.asDomainModel() = ArticleDo(
     id = this.id,
     title = this.title,
-    artistDisplay = this.artistDisplay,
-    imageUrl = this.imageUrl,
     copy = this.copy,
 )
