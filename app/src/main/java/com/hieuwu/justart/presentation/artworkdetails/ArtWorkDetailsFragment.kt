bf1299f46993b6dfc27d428678709173ff5a3da2
Hieu Vu <star.hieu.99@gmail.com>
2022-11-16 22:14:36 +0700

    Remove unused code and comments

--- app/src/main/java/com/hieuwu/justart/presentation/artworkdetails/ArtWorkDetailsFragment.kt
+++ app/src/main/java/com/hieuwu/justart/presentation/artworkdetails/ArtWorkDetailsFragment.kt
@@ -167,12 +167,6 @@ class ArtWorkDetailsFragment : Fragment() {
             } else {
                 binding.toolbar.menu.getItem(0).setIcon(R.drawable.ic_outline_favorite_app_bar)
             }
-
-//            data?.let {
-//                binding.toolbar.menu.getItem(0).setIcon(R.drawable.ic_baseline_favorite_app_bar)
-//                return@launch
-//            }
-//            binding.toolbar.menu.getItem(0).setIcon(R.drawable.ic_outline_favorite_app_bar)
         }
     }
 
